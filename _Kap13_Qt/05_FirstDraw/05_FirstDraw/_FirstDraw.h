d674e9f04cad13c1d8d8a60b224878189a9c2ae7
Prof. Just <olaf.just@w-hs.de>
2020-11-25 12:53:49 +0100

    Add files via upload

---
+++ _Kap13_Qt/05_FirstDraw/05_FirstDraw/_FirstDraw.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <QtWidgets/QMainWindow>
+#include "ui__FirstDraw.h"
+#include <QPainter>
+#include <QStaticText>
+#include <QFont>
+
+class _FirstDraw : public QMainWindow
+{
+	Q_OBJECT
+
+public:
+	_FirstDraw(QWidget *parent = Q_NULLPTR);
+	QPainter p;  //Instanz von Painter erstellen
+	void _FirstDraw::paintEvent(QPaintEvent *e);
+	void _FirstDraw::zeichne();
+	
+private:
+	Ui::_FirstDrawClass ui;
+};
